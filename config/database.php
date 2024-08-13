26cff5e196d3c630dbb3b47b34e3c19e98dcc72d
Reniel V. Mallari <38750825+reinmallari@users.noreply.github.com>
2024-08-13 21:37:45 +0800

    redis cache tls

--- config/database.php
+++ config/database.php
@@ -138,12 +138,12 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'scheme' => 'tls',
+            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
+            'port' => env('AZURE_REDIS_PORT', '6379'),
+            'database' => env('AZURE_REDIS_DATABASE', '1'),
         ],
 
     ],
